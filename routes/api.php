bce191434f6c61c4290d3f414bba8317c2c4607e
RumpaPaul <rumpakucse14@gmail.com>
2021-08-12 12:06:49 +0600

    created route for article-controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('/articles')->group(function(){
+   Route::get('/index', [ArticleController::class, 'index']);
+   Route::post('/store', [ArticleController::class, 'store']);
+   Route::get('/show/{id}', [ArticleController::class, 'show']);
+   Route::put('/update/{id}', [ArticleController::class, 'update']);
+   Route::delete('/delete/{id}', [ArticleController:: class, 'destroy']);
 });
